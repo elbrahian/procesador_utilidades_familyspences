26aa67d9bbcdebe3c9b4990ff2f146e33b0ab3b1
Brahian Rueda Gutierrez <brahianrueda123@gmail.com>
2025-10-04 11:23:56 -0500

    initial commit

---
+++ src/test/java/com/familyspences/procesador_utilidades_api/ProcesadorUtilidadesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familyspences.procesador_utilidades_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesadorUtilidadesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
